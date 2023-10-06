e563001a4544f36d873209d886071cb4c58e8897
hempan9 <hemantapant9@gmail.com>
2023-10-06 11:34:25 -0500

    code cleanup

--- src/main/java/com/hmp/reactive/repository/UserRepository.java
+++ src/main/java/com/hmp/reactive/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.hmp.reactive.repository;
 
 import com.hmp.reactive.model.documents.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 @Repository
